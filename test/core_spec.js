bf5e04f2cf788debef8e6cb49dfbffec1f03743a
redhendery <redhendery@gmail.com>
2016-04-27 14:20:33 +1200

    add src folder with core.js file

--- test/core_spec.js
+++ test/core_spec.js
@@ -4,10 +4,18 @@ import {expect} from 'chai';
 import {setEntries} from '../src/core';
 
 describe('application logic', () => {
-  const state = Map();
-  const entries = List.of('Trainspotting', '28 Days Later');
-  const nextEntries = setEntries(state, entries);
-  expect(nextState).to.equal(Map({
-    entries: List.of('Trainspotting', '28 Days Later')
-  }));
+
+  describe('setEntries', () => {
+
+    it('adds the entries to the state', () => {
+      const state = Map();
+      const entries = List.of('Trainspotting', '28 Days Later');
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+
+  });
+
 });
